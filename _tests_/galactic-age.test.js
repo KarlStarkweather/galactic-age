837670705090711352eae443a9063f2826b35f14
KarlStarkweather <stark13@usa.net>
2021-12-30 15:55:56 -0800

    initial commit with file structure in place

---
+++ _tests_/galactic-age.test.js
@@ -0,0 +1,11 @@
+import GalacticAge from '../src/galactic-age.js';
+
+describe('Age', () => {
+
+  test('should correctly calculate standard age from a birthdate', () => {
+    const age = new GalacticAge(7/25/1969);
+    expect(age.solarAge).toEqual(52);
+  });
+
+
+});
---
+++ src/galactic-age.js
@@ -0,0 +1,10 @@
+export default class GalacticAge {
+
+  constructor(birthDate) {
+    this.solarAge = 0;
+  };
+
+  const getAge = (planet) => {
+    return("don't know");
+  };
+};
