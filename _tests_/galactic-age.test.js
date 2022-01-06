cb4dcb866ea0a785ed81c82c111f4d521a211952
KarlStarkweather <stark13@usa.net>
2022-01-05 18:45:54 -0800

    change test for exceeding life expectancy to apply to mars

--- _tests_/galactic-age.test.js
+++ _tests_/galactic-age.test.js
@@ -4,19 +4,21 @@ describe('Age', () => {
 
   test('should correctly calculate standard age from a birthdate', () => {
     const age = new GalacticAge("7/25/1969");
-    expect(age.getEarthAge()).toEqual(52.4);
+    expect(age.getEarthAge()).toEqual(52.5);
   });
   test('should correctly convert the age for any planet in our earth system', () => {
     const age = new GalacticAge("7/25/1969");
     age.getEarthAge();
     expect(age.getPlanetAge("Mars")).toEqual(27.9)
   })
-  test('should correctly calculate the users remaining life expectancy on a given planet', () => {
+  test('should correctly calculate the users remaining life expectancy on Mars', () => {
     const age = new GalacticAge("7/25/1969");
     expect(age.expectedYearsRemaining("Mars")).toEqual(-14.5)
   })
   test('should calculate remaining life expectancy on a given planet as a positive number if user has exceeded it', () => {
     const age = new GalacticAge("1/1/1922");
-    expect(age.expectedYearsRemaining("Mercury")).toEqual(84.2)
+    expect(age.expectedYearsRemaining("Mars")).toEqual(84.2)
   })
+  
+
 });
