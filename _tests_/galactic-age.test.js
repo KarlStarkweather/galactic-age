d46c6a1e1acfe05852a4ce03009fa92f152eaf24
KarlStarkweather <stark13@usa.net>
2022-01-05 18:47:59 -0800

    good fail of test for  life expectancy on Venus

--- _tests_/galactic-age.test.js
+++ _tests_/galactic-age.test.js
@@ -18,7 +18,10 @@ describe('Age', () => {
   test('should calculate remaining life expectancy on a given planet as a positive number if user has exceeded it', () => {
     const age = new GalacticAge("1/1/1922");
     expect(age.expectedYearsRemaining("Mars")).toEqual(10.8)
+  }) 
+  test('should correctly calculate the users remaining life expectancy on Venus', () => {
+    const age = new GalacticAge("7/25/1969");
+    expect(age.expectedYearsRemaining("Venus")).toEqual(-14.5)
   })
-  
 
 });
